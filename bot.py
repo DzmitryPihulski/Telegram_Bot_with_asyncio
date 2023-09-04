9a2ab4bb3282c0f6493a4bc015a1dd1169d07fd1
PIHU <dim.pigulsky@gmail.com>
2023-09-04 09:23:04 +0200

    QR codes added

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import asyncio
 import logging
 import sys
 import requests
+import pyqrcode
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.methods.send_message import SendMessage
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup,ReplyKeyboardRemove,KeyboardButton,Message, CallbackQuery
+from aiogram.types import ReplyKeyboardMarkup,ReplyKeyboardRemove,KeyboardButton,Message, CallbackQuery,FSInputFile
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.utils.markdown import hbold
@@ -23,7 +24,7 @@ TOKEN = os.getenv('TOKEN')
 zodiac_signs=['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
 dp = Dispatcher()
 
-keybord=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Weather'),KeyboardButton(text='Horoscope')]])
+keybord=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Weather'),KeyboardButton(text='Horoscope')],[KeyboardButton(text='Ask AI')]])
 
 def gen_keybord(args):
     list=[]
@@ -35,6 +36,7 @@ class Form(StatesGroup):
     city = State()
     zodiac_sign=State()
     horoscope_date=State()
+    ai_querry=State()
     
 def gen_markup(args):
     markup = InlineKeyboardBuilder()
@@ -99,7 +101,10 @@ async def get_weather(message: Message):
 @dp.message()
 async def echo_handler(message: Message) -> None:
     try:
-        await message.answer('I do not understand you.')
+        img=pyqrcode.create(message.text)
+        img.png('qrcode.png',scale=5)
+        await message.answer_photo(photo=FSInputFile('qrcode.png'),caption='I do not understand you, but here is qr-code of your message',parse_mode='Markdown')
+        os.remove('qrcode.png')
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
